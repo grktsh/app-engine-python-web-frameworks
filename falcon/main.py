0410858f79985647fd4328d37a52cf3ad867e5ae
grktsh <grktsh@gmail.com>
2018-07-28 22:54:20 +0900

    Use falcon.Response.media

--- falcon/main.py
+++ falcon/main.py
@@ -1,10 +1,9 @@
 import falcon
-from falcon.util import json
 
 
 class Hello(object):
     def on_get(self, req, resp, name):
-        resp.body = json.dumps({'hello': name})
+        resp.media = {'hello': name}
 
 
 app = falcon.API()
